cb9d612deca11fdd0e2088e40504e66748b75954
jackycklo <jackycklo2@gmail.com>
2017-04-30 19:58:08 +0800

    add project

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Chi kit Lo on 30/4/2017.
+//  Copyright © 2017 Chi kit Lo. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+    
+    
+    
+
+}
+
